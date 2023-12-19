8e98598eca00dbf5bdedf66703713bcfca559338
Matthieu Flce <flacelierematthieu@gmail.com>
2023-12-19 10:14:26 +0100

    First commit

---
+++ server/src/main/java/fr/esgi/zonevaluator/ZonevaluatorApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.zonevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonevaluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZonevaluatorApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/fr/esgi/zonevaluator/ZonevaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.zonevaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZonevaluatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
