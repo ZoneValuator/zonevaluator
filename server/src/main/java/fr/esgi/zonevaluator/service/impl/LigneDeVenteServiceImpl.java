69b1cd7e8906c697fb1451ae09dee20a629af62b
Matthieu Flce <flacelierematthieu@gmail.com>
2023-12-22 16:37:47 +0100

    feat: impl recupererLignesDeVenteByLocation

--- server/src/main/java/fr/esgi/zonevaluator/service/impl/LigneDeVenteServiceImpl.java
+++ server/src/main/java/fr/esgi/zonevaluator/service/impl/LigneDeVenteServiceImpl.java
@@ -14,14 +14,54 @@ public class LigneDeVenteServiceImpl implements LigneDeVenteService {
 
     private LigneDeVenteRepository ligneDeVenteRepository;
 
-    /**
-     * @param latitude 
-     * @param longitude 
-     * @return
-     */
-    public List<LigneDeVente> recupererLignesDeVenteByLocation(Float latitude, Float longitude) {
-        // TODO implement here
-        return null;
+    @Override
+    public List<LigneDeVente> recupererLignesDeVenteByLocation(Float latitude, Float longitude, Float rayon) {
+        // Récupération des lignes de vente
+        List<LigneDeVente> lignesDeVente = ligneDeVenteRepository.findAll();
+
+        // Création d'une liste pour stocker les lignes de vente dans le rayon
+        List<LigneDeVente> lignesDeVenteDansLeRayon = new ArrayList<>();
+
+        // Pour chaque ligne de vente, on calcule la distance entre la ligne de vente et le point GPS de l'utilisateur
+        for (LigneDeVente ligneDeVente : lignesDeVente) {
+            float latitudeLigneDeVente = ligneDeVente.getLatitude();
+            float longitudeLigneDeVente = ligneDeVente.getLongitude();
+            // Calcul de la distance avec la formule de Haversine
+            double distance = haversine_distance(latitude, longitude, latitudeLigneDeVente, longitudeLigneDeVente);
+            // Si la distance est inférieure ou égale au rayon, on ajoute la ligne de vente à la liste
+            if (distance <= rayon) {
+                lignesDeVenteDansLeRayon.add(ligneDeVente);
+            }
+        }
+
+        return lignesDeVenteDansLeRayon;
+    }
+
+    private Double haversine_distance(float lat1, float lng1, float lat2, float lng2) {
+        // Calcul de la distance
+        float radius = 6371f; // Rayon de la terre en km
+        float dlat = deg2rad(lat2 - lat1);
+        float dlng = deg2rad(lng2 - lng1);
+
+        // Formule de Haversine
+        // https://en.wikipedia.org/wiki/Haversine_formula
+        // https://stackoverflow.com/a/27943
+        double a =
+                haversine(dlat) +
+                Math.cos(deg2rad(lat1)) *
+                Math.cos(deg2rad(lat2)) *
+                haversine(dlng);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return radius * c; // Distance en Km
+    }
+
+    private double haversine(float latInRadian) {
+        return Math.pow(Math.sin(latInRadian / 2), 2);
+    }
+
+    private float deg2rad(float deg) {
+        return deg * (float) (Math.PI / 180);
     }
 
     @Override
