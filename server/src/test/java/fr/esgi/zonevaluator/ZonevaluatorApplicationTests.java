4db2636eda921e518980b7add5bee71de874fa2b
Matthieu Flce <flacelierematthieu@gmail.com>
2024-02-06 12:30:36 +0100

    test: add unit test

--- server/src/test/java/fr/esgi/zonevaluator/ZonevaluatorApplicationTests.java
+++ server/src/test/java/fr/esgi/zonevaluator/ZonevaluatorApplicationTests.java
@@ -2,12 +2,44 @@ package fr.esgi.zonevaluator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+		classes = ZonevaluatorApplication.class)
+
+@AutoConfigureMockMvc
 class ZonevaluatorApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void testGeneratePdfByLocationEndpoint() throws Exception {
+			mockMvc.perform(MockMvcRequestBuilders.get("/generatePdfByLocation")
+							.param("latitude", "40.7128")
+							.param("longitude", "-74.0060")
+							.param("rayon", "10")
+							.contentType(MediaType.APPLICATION_JSON))
+					.andExpect(MockMvcResultMatchers.status().isCreated())
+					.andDo(print());
 	}
 
+	@Test
+	void testGeneratePdfByLocationEndpointWithoutLatitude() throws Exception {
+			mockMvc.perform(MockMvcRequestBuilders.get("/generatePdfByLocation")
+							.param("longitude", "-74.0060")
+							.param("rayon", "10")
+							.contentType(MediaType.APPLICATION_JSON))
+					.andExpect(MockMvcResultMatchers.status().isBadRequest())
+					.andDo(print());
+	}
 }
